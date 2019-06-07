065e209aae6bf3badcbf543967f13afe87000147
Carlos E Carrillo <carloscarrilloce@gmail.com>
2019-06-07 15:41:51 -0500

    typing v1 fix

---
+++ typings/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for epaisa-components
+// Project: https://github.com/carrillocarloscebits/epaisa-components
+// Definitions by: Carlos E Carrillo 
+// TypeScript Version: 3.5.1
+
+import * as React from "react";
+import { ViewStyle, TextStyle, TextProps } from "react-native";
+
+import { TextMontserratProps } from "../src/TextMontserrat/TextMontserrat";
+import { EpaisaPaymentButtonProps } from "../src/EpaisaGradientButton/EpaisaGradientButton";
+import { EpaisaCircleButtonProps } from "../src/EpaisaCircleButton/EpaisaCircleButton";
+import { EpaisaSimpleInputProps } from "../src/EpaisaSimpleInput/EpaisaSimpleInput";
+
+export declare const TextMontserrat: React.SFC<TextMontserratProps>
+export declare const EpaisaPaymentButton: React.SFC<EpaisaPaymentButtonProps>
+export declare const EpaisaCircleButton: React.SFC<EpaisaCircleButtonProps>
+export declare const EpaisaCircleButtonProps: React.SFC<EpaisaSimpleInputProps>
